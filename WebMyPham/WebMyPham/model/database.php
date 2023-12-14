06e43e2dd47da3dd0e27918c8c95517d2aa3616a
thanhphugo <thanhphulol2510@gmail.com>
2023-12-14 14:09:23 +0700

    phu

--- WebMyPham/WebMyPham/model/database.php
+++ WebMyPham/WebMyPham/model/database.php
@@ -3,7 +3,7 @@ class DATABASE
 {
     private static $dns = "mysql:host=localhost;dbname=shop;port=3306";
     private static $username = "root";
-    private static $password = "";
+    private static $password = "vertrigo";
     private static $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
     private static $db;
